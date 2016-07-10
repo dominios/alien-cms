2cfefe6c916026c4c3c11805f841e54e72efb8ab
Dominik Gersak <domgersak@gmail.com>
2016-07-10 09:37:32 +0200

    removed manually downloaded & not needed assets

---
+++ public/src/js/_cms.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('AlienCms', [
+    'ngResource',
+    'notifications'
+]).directive('ckEditor', [function () {
+    return {
+        require: '?ngModel',
+        link: function ($scope, elm, attr, ngModel) {
+
+            var ck = CKEDITOR.inline(elm[0]);
+
+            ck.on('pasteState', function () {
+                $scope.$apply(function () {
+                    ngModel.$setViewValue(ck.getData());
+                });
+            });
+
+            ngModel.$render = function (value) {
+                ck.setData(ngModel.$modelValue);
+            };
+        }
+    };
+}]);
