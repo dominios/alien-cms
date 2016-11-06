3424c9037b301c56543d2759e15ece819839cb88
Dominik Gersak <domgersak@gmail.com>
2016-11-06 10:54:45 +0100

    cleaning user api

--- public/src/js/models/user/api.js
+++ public/src/js/models/user/api.js
@@ -1,56 +1,24 @@
-define([
-    './factory'
-], function (User) {
-
+define([], function () {
     'use strict';
-
-    // angular
-    //     .module('AlienCms.Models.User')
-        // .factory('User', UserFactory)
-        // .service('UserManager', UserManager)
-        // .service('UserApi', UserApi);
 
     UserApi.$inject = ['$resource', 'User'];
     function UserApi ($resource, User) {
-        
+
         var Interceptors = {
-            response: function(response) {
+            response: function (response) {
                 var user = new User();
                 user.setData(response.data.data);
                 return user;
             }
         };
-        
+
         return $resource('api/v1/users/id/:id', {
             id: '@id'
         }, {
-            get: { method: 'GET', interceptor: Interceptors }
+            get: { method: 'GET', interceptor: Interceptors, cache: true }
         });
     }
 
     return UserApi;
-
-    //
-    // UserManager.$inject = ['UserApi', 'User'];
-    // function UserManager(UserApi, User) {
-    //
-    //     var UserManager = {
-    //
-    //         _pool: {},
-    //
-    //         _retrieveInstance: function(id, data) {
-    //             var instance = this._pool[id];
-    //             if (instance) {
-    //                 instance.setData(data);
-    //             } else {
-    //                 instance = new User();
-    //                 instance.setData(data)
-    //                 this._pool[id] = instance;
-    //             }
-    //             return instance;
-    //         }
-    //
-    //     };
-    // }
 
 });
